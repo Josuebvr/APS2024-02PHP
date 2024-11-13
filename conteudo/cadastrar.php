ee32089649195bb084a026878597d5aacefc708e
Josué Rios <josuebvrios@gmail.com>
2024-11-13 06:37:21 -0300

    teste de criptografia da senha

--- conteudo/cadastrar.php
+++ conteudo/cadastrar.php
@@ -1,13 +1,15 @@
 <?php
-
     include("conexao.php");
+
+    // Inicialize a variável $erro como array vazio
+    $erro = [];
 
     if(isset($_POST['confirmar'])) {    
     
         if(!isset($_SESSION)) 
             session_start();
         
-        foreach($_POST as $chave=>&$valor) 
+        foreach($_POST as $chave => &$valor) 
             $_SESSION[$chave] = $mysqli->real_escape_string($valor);
 
         if(strlen($_SESSION['nome']) == 0)
@@ -28,10 +30,11 @@
         if(strcmp($_SESSION['senha'], $_SESSION['rsenha']) != 0)
             $erro[] = "As senhas não batem.";
 
-
         if(count($erro) == 0){
 
-            $slq_code = "INSERT INTO usuario (
+            $senha = md5(md5($_SESSION['senha']));
+
+            $sql_code = "INSERT INTO usuario (
                 nome, 
                 sobrenome, 
                 email, 
@@ -46,33 +49,28 @@
                 '$_SESSION[senha]',
                 '$_SESSION[sexo]',
                 '$_SESSION[niveldeacesso]',
-                '$_SESSION[datadecadastro]'
+                NOW()
                 )";
 
-            $confirma = $mysqli->query($slq_code) or die ($mysqli->error);
+            $confirma = $mysqli->query($sql_code) or die ($mysqli->error);
 
             if($confirma){
-
-                unset($_SESSION['nome'],
-                $_SESSION['sobrenome'],
-                $_SESSION['email'],
-                $_SESSION['senha'],
-                $_SESSION['sexo'],
-                $_SESSION['niveldeacesso'],
-                $_SESSION['datadecadastro']);
-
+                unset($_SESSION['nome'], 
+                      $_SESSION['sobrenome'], 
+                      $_SESSION['email'], 
+                      $_SESSION['senha'], 
+                      $_SESSION['sexo'], 
+                      $_SESSION['niveldeacesso'], 
+                      $_SESSION['datadecadastro']);
+                
                 echo "<script> location.href='index.php?p=inicial'; </script>";
-
-            }else
+            } else {
                 $erro[] = "Erro ao cadastrar: " . $mysqli->error;
-
+            }
         }
     }
-
 ?>
 
-
- 
 <h1>Cadastrar Usuário</h1>
 <?php 
 if(count($erro) > 0){ 
@@ -124,5 +122,5 @@ if(count($erro) > 0){
     <p class=espaco></p>
 
     <input value="Salvar" name="confirmar" type="submit">
-
 </form>
+
